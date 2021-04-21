0ae150afc30d6af6be33fddb1ccacdb44e6f9478
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-21 22:35:31 +0200

    så

---
+++ show.js
@@ -0,0 +1,31 @@
+var slideIndex = 0;
+showSlides();
+var timer = null;
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slides");
+  var dots = document.getElementsByClassName("mini-img");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+
+  if (slideIndex > slides.length) {
+    slideIndex = 1
+  }    
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  timer = setTimeout(showSlides, 6000);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  clearTimeout(timer);
+}
