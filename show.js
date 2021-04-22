57a6522d83609fecbcec1052c9cec6dd41fcb4fb
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-22 22:44:12 +0200

    Animation YESSS

--- show.js
+++ show.js
@@ -1,21 +1,3 @@
 function menu(x) {
   x.classList.toggle("change");
 }
-
-var  abox = document.getElementsByClassName("box")[0];
-
-function allmove(){
-      abox.classList.remove("move-ltr");
-      abox.classList.remove("move-ttb");
-      abox.classList.toggle("move");
-}
-function ltr(){
-      abox.classList.remove("move");
-      abox.classList.remove("move-ttb");
-      abox.classList.toggle("move-ltr");
-}
-function ttb(){
-    abox.classList.remove("move-ltr");
-    abox.classList.remove("move");
-    abox.classList.toggle("move-ttb");
-}
