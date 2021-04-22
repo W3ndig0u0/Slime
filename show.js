2fa75cac270e2cccadb59f3114f1c32a0a668cbc
W3ndig0u0 <jingxiang.xu@elev.ga.ntig.se>
2021-04-22 11:06:38 +0200

    animation

--- show.js
+++ show.js
@@ -1,35 +1,21 @@
-var slideIndex = 0;
-showSlides();
-var timer = null;
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slides");
-  var dots = document.getElementsByClassName("mini-img");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-
-  if (slideIndex > slides.length) {
-    slideIndex = 1
-  }    
-
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  timer = setTimeout(showSlides, 10000);
-}
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-  clearTimeout(timer);
-}
-
 function menu(x) {
   x.classList.toggle("change");
+}
+
+var  abox = document.getElementsByClassName("box")[0];
+
+function allmove(){
+      abox.classList.remove("move-ltr");
+      abox.classList.remove("move-ttb");
+      abox.classList.toggle("move");
+}
+function ltr(){
+      abox.classList.remove("move");
+      abox.classList.remove("move-ttb");
+      abox.classList.toggle("move-ltr");
+}
+function ttb(){
+    abox.classList.remove("move-ltr");
+    abox.classList.remove("move");
+    abox.classList.toggle("move-ttb");
 }
